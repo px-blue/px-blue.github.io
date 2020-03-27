cc72163b2d406b05392c0e2eef308009225e1845
joebochill <boyle.p.joseph@gmail.com>
2020-03-27 17:36:11 -0400

    Manual deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bc32d01da2792d1c9155d679191cf81.js"
+  "/precache-manifest.258022d04ccedfeb8bea303129c8fb16.js"
 );
 
 self.addEventListener('message', (event) => {
